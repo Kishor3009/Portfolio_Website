579860789de3ce5317c884b686699bca0a6f3ab8
Kishor3009 <119849236+Kishor3009@users.noreply.github.com>
2024-04-23 14:37:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
       <Cursor />
       <section id="Homepage">
         <Navbar />
-        <Hero />
+        
       </section>
       <section id="Services">
         <Parallax type="services" />
